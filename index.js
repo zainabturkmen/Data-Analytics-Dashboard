4cb54c43160b17940d1470c200773275cb48d931
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-15 14:45:32 +0430

    init

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // javascript
+
+const menuBtn = document.querySelector('.menu-btn')
+const backBtn = document.querySelector('.back-btn')
+const menu = document.querySelector('nav')
+
+menuBtn.addEventListener('click', () => {
+    menu.style.transform = 'translateX(0)';
+})
